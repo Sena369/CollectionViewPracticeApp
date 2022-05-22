9fa2a23d77de37ce7bbd065ba372038bc50710ad
Sena <senana0116@icloud.com>
2022-05-22 19:03:29 +0900

    first commit

---
+++ CollectionViewPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewPracticeApp
+//
+//  Created by 澤田世那 on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
